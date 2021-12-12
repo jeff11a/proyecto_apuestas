f10af1a21de1c36943077db0600778794b2b9c40
Oscar Pérez S <ingoscarperezes@gmail.com>
2021-12-12 17:40:58 -0500

    Cambios menores

--- frontend/src/services/BetService.js
+++ frontend/src/services/BetService.js
@@ -1,7 +1,7 @@
 import http from "../http-common";
 
-const getAll = () => {
-  return http.get("/bets");
+const getAll = (params) => {
+  return http.get("/bets", { params });
 };
 
 const get = (id) => {
